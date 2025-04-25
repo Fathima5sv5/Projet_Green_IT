9bf6659bab179d4ecfa96d8dbc13b4e852d23e8c
youfavlena <lm244400@gmail.com>
2025-04-25 19:31:45 +0200

    seccess

--- handy-ecommerce/frontend/src/router/index.js
+++ handy-ecommerce/frontend/src/router/index.js
@@ -5,13 +5,15 @@ import Login from '../pages/Login.vue';
 import Register from '../pages/Register.vue';
 import Product from "../pages/Product.vue";
 import Tableware from "../pages/Tableware.vue";
+import Artist from "../pages/Artist.vue"
 
 const routes = [
-  { path: '/', component: Product },
+  { path: '/', component: Tableware },
   { path: '/login', component: Login },
   { path: '/register', component: Register },
   { path: '/product', component: Product },
   { path : '/tableware', component: Tableware},
+  { path: '/artist', component: Artist},
 ];
 
 const router = createRouter({
