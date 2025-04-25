1f49468e04a0a0450232fe56f735e210a39c0db0
Mel <melissatalbi13@gmail.com>
2025-04-25 19:24:48 +0200

    ajout comp tablexare

--- handy-ecommerce/frontend/src/router/index.js
+++ handy-ecommerce/frontend/src/router/index.js
@@ -4,12 +4,14 @@ import Home from '../pages/Home.vue';
 import Login from '../pages/Login.vue';
 import Register from '../pages/Register.vue';
 import Product from "../pages/Product.vue";
+import Tableware from "../pages/Tableware.vue";
 
 const routes = [
   { path: '/', component: Product },
   { path: '/login', component: Login },
   { path: '/register', component: Register },
   { path: '/product', component: Product },
+  { path : '/tableware', component: Tableware},
 ];
 
 const router = createRouter({
